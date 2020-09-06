acf1ee693c770f2c4d6e3086205f10002fc75eb5
zydricDev <70192206+zydricDev@users.noreply.github.com>
2020-09-06 18:43:56 -0400

    Initial commit

---
+++ app/Http/Controllers/ProfilesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfilesController extends Controller
+{
+  public function index($user)
+  {
+      $user = \App\User::findOrFail($user);
+      return view('home', [
+        'user' => $user,
+      ]);
+  }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col-3 p-5">
+      <img src="https://images.unsplash.com/photo-1599205940775-6eb9db72cbce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" class="rounded-circle" style="height: 13em; width: 13em;">
+    </div>
+    <div class="col-9 pt-5 ">
+      <div><h1>{{ $user->username }}</h1></div>
+      <div class="d-flex">
+        <div class="pr-5"><strong>153</strong> posts</div>
+        <div class="pr-5"><strong>153k</strong> followers</div>
+        <div class="pr-5"><strong>153gp</strong> following</div>
+      </div>
+      <div class="pt-4 font-weight-bold">{{ $user->profile->title}}</div>
+      <div>{{ $user->profile->description }}</div>
+      <div><a href="#">{{ $user->profile->url ?? 'N/a'}}</a></div>
+    </div>
+  </div>
+
+</div>
+@endsection
