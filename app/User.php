42158743e52f9ddd71055acf87c219bd402377ed
zydricDev <70192206+zydricDev@users.noreply.github.com>
2020-09-07 04:43:35 -0400

    3 hours in
    3 hours in

--- app/User.php
+++ app/User.php
@@ -36,6 +36,17 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    protected static function boot()
+    {
+      parent::boot();
+
+      static::created(function($user){
+          $user->profile()->create([
+            'title' => $user->username,
+          ]);
+      });
+    }
+
     public function posts()
     {
       return $this->hasMany(Post::class)->orderBy('created_at','DESC');
@@ -45,4 +56,6 @@ class User extends Authenticatable
     {
         return $this->hasOne(Profile::class);
     }
+
+
 }
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,16 +9,24 @@
     <div class="col-9 pt-5 ">
       <div class="d-flex justify-content-between align-items-baseline">
         <h1>{{ $user->username }}</h1>
-        <a href="/p/create">Add new post </a>
+
+        @can('update',$user->profile)
+          <a href="/p/create">Add new post </a>
+        @endcan
+
 
       </div>
-      <a href="#">Edit Profile</a>
+
+      @can('update',$user->profile)
+      <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
+      @endcan
+
       <div class="d-flex">
         <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
         <div class="pr-5"><strong>153k</strong> followers</div>
         <div class="pr-5"><strong>153gp</strong> following</div>
       </div>
-      <div class="pt-4 font-weight-bold">{{ $user->profile->title}}</div>
+      <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
       <div>{{ $user->profile->description }}</div>
       <div><a href="#">{{ $user->profile->url ?? 'N/a'}}</a></div>
     </div>
