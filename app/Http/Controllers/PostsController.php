200be1fd39b290da127de388befce55dad809c49
zydricDev <70192206+zydricDev@users.noreply.github.com>
2020-09-07 02:10:15 -0400

    Edit section

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class PostsController extends Controller
 {
@@ -24,12 +25,23 @@ class PostsController extends Controller
         'image' => ['required', 'image'],
       ]);
 
-      auth()->user()->posts()->create($data);
+      $imagePath= request('image')->store('uploads', 'public');
 
+      $image = Image::make(public_path("storage/{$imagePath}"))->fit(1200, 1200);
+      $image->save();
 
+      auth()->user()->posts()->create([
+        'caption' => $data['caption'],
+        'image' => $imagePath,
+      ]);
 
-      dd(request()->all());
+      return redirect('/profile/'.auth()->user()->id);
 
+    }
+
+    public function show(\App\Post $post)
+    {
+      return view('posts.show', compact('post'));
     }
 
 }
